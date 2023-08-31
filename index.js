2ea26183d24b516217e6a777456ed9aa51ad76ac
10Sadique <jafarsjahan@gmail.com>
2023-08-31 10:49:09 +0600

    todo app  complete

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-import { connectDB, disconnectDB } from "./db/connectDB.js";
+#!/usr/bin/env node
 
-console.log("Hello CodeSandbox");
+import { Command } from "commander";
 
-connectDB();
-disconnectDB();
+import addTask from './commands/addTask.js';
+import readTask from './commands/readTask.js';
+import updateTask from './commands/updateTask.js';
+import deleteTask from './commands/deleteTask.js'
+
+const program = new Command();
+
+program
+  .name("todo")
+  .description("Your terminal task manager!")
+  .version("1.0.0");
+
+program.name("add").description("Create a new todo.").action(addTask);
+
+program.name("read").description("Reads all the todo.").action(readTask);
+
+program.name("update").description("Updates a todo.").action(updateTask);
+
+program.name("delete").description("Deletes a todo.").action(deleteTask);
+
+program.parse();
