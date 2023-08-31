a047a9f853ec23ce781e068f2830a6d14fe0f976
10Sadique <jafarsjahan@gmail.com>
2023-08-31 10:51:37 +0600

    bug fix

--- commands/addTask.js
+++ commands/addTask.js
@@ -1,58 +1,66 @@
-import ora from 'ora';
-import chalk from 'chalk';
-import inquirer from 'inquirer';
-import Todos from '../schema/todoSchema.js';
-import { connectDB, disconnectDB } from '../db/connectDB.js';
+import ora from "ora";
+import chalk from "chalk";
+import inquirer from "inquirer";
+import Todos from "../schema/todoSchema.js";
+import { connectDB, disconnectDB } from "../db/connectDB.js";
 
-async function input () {
-    const answers = await inquirer.prompt([
-        { name: 'name', message: 'Enter name of the task:', type: 'input' },
-        { name: 'detail', message: 'Enter the details of the task:', type: 'input' },
-    ]);
+async function input() {
+  const answers = await inquirer.prompt([
+    { name: "name", message: "Enter name of the task:", type: "input" },
+    {
+      name: "detail",
+      message: "Enter the details of the task:",
+      type: "input",
+    },
+  ]);
 
-    return answers;
+  return answers;
 }
 
 const askQuestions = async () => {
-    const todoArray = [];
-    let loop = false;
+  const todoArray = [];
+  let loop = false;
 
-    do {
-        const userRes = await input();
-        todoArray.push(userRes);
+  do {
+    const userRes = await input();
+    todoArray.push(userRes);
 
-        const confirmQ = await inquirer.prompt([
-            { name: 'confirm', message: 'Do you want to add more tasks?', type: 'confirm' }
-        ]);
+    const confirmQ = await inquirer.prompt([
+      {
+        name: "confirm",
+        message: "Do you want to add more tasks?",
+        type: "confirm",
+      },
+    ]);
 
-        if (confirmQ.confirm) {
-            loop = true;
-        } else {
-            loop = false;
-        }
-    } while (loop);
-
-    return todoArray;
-}
-
-export default async function addTask () {
-    try {
-        const userResponse = await askQuestions();
-        await connectDB();
-
-        let spinner = ora('Creating the todo...').start();
-
-        for (let i = 0; i < userResponse.length; i++) {
-            const res = userResponse[i];
-            await Todos.create(res);
-        }
-
-        spinner.stop();
-        console.log(chalk.greenBright('Ctreatd the todos!'));
-
-        await disconnectDB();
-    } catch (error) {
-        console.log('Something went wrong, Error: ', error);
-        process.exit(1);
+    if (confirmQ.confirm) {
+      loop = true;
+    } else {
+      loop = false;
     }
+  } while (loop);
+
+  return todoArray;
+};
+
+export default async function addTask() {
+  try {
+    const userResponse = await askQuestions();
+    await connectDB();
+
+    let spinner = ora("Creating the todo...").start();
+
+    for (let i = 0; i < userResponse.length; i++) {
+      const res = userResponse[i];
+      await Todos.create(res);
+    }
+
+    spinner.stop();
+    console.log(chalk.greenBright("Created the todos!"));
+
+    await disconnectDB();
+  } catch (error) {
+    console.log("Something went wrong, Error: ", error);
+    process.exit(1);
+  }
 }
--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
+
 
 import addTask from './commands/addTask.js';
 import readTask from './commands/readTask.js';
 import updateTask from './commands/updateTask.js';
 import deleteTask from './commands/deleteTask.js'
 
-const program = new Command();
+import { Command } from 'commander'
 
+// Creating an instance of the Command class
+const program = new Command()
+
+// Setting the name and description of the CLI tool
 program
-  .name("todo")
-  .description("Your terminal task manager!")
-  .version("1.0.0");
+.name('todo')
+.description('Your terminal task manager!')
+.version('1.0.0')
 
-program.name("add").description("Create a new todo.").action(addTask);
+// Defining a command called 'add'
+program
+.command('add')
+.description('Create a new todo.')
+.action(addTask)
 
-program.name("read").description("Reads all the todo.").action(readTask);
+// Defining a command called 'read'
+program
+.command('read')
+.description('Reads all the todos.')
+.action(readTask)
 
-program.name("update").description("Updates a todo.").action(updateTask);
+// Defining a command called 'update'
+program
+.command('update')
+.description('Updates a todo.')
+.action(updateTask)
 
-program.name("delete").description("Deletes a todo.").action(deleteTask);
+// Defining a command called 'delete'
+program
+.command('delete')
+.description('Deletes a todo.')
+.action(deleteTask)
 
-program.parse();
+// Parsing the command-line arguments and executing the corresponding actions
+program.parse()
